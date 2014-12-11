6bc257b3e4e8fbb11cfd86fa27ec6acc5ee41815
Mark <markmaw@gmail.com>
2014-12-11 03:16:30 -0500

    format attributes and print status

--- processing/CreateDocuments.py
+++ processing/CreateDocuments.py
@@ -1,43 +1,89 @@
 import sys
 import os
+import re
 import json
 import pandas as pd
+import subprocess
 
-Businesses = pd.DataFrame(columns=['business_id', 'name', 'latitude', 'longitude',  'stars', 'full_address','categories', 'attributes'])
+Businesses = pd.DataFrame(columns=['business_id', 'name', 'latitude', 'longitude',  'stars', 'full_address', 'postcode', 'categories', 'attributes'])
 Reviews = pd.DataFrame(columns=['business_id','text', 'votes'])
 Tips = pd.DataFrame(columns=['business_id','text', 'likes'])
 
+#USPOSTCODE = re.compile("\d{5}(?:[-\s]\d{4})?")
+#UKPOSTCODE = re.compile("[A-Z]{1,2}[0-9R][0-9A-Z]?\s+[0-9][ABD-HJLNP-UW-Z]{2}")
+#CAPOSTCODE = re.compile("[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}")
+POSTCODE = re.compile("\d{5}(?:[-\s]\d{4})?|[A-Z]{1,2}[0-9R][0-9A-Z]?\s+[0-9][ABD-HJLNP-UW-Z]{2}|[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}")
+
 def read_businesses(filename):
+    global Businesses
+    total = file_len(filename)
     with open(filename, 'r') as fp:
-        for line in fp.readlines():
+        for i, line in enumerate(fp.readlines()):
             obj = json.loads(line)
-            b_id = obj['business_id']
-            name = obj['name']
-            lati = obj['latitude']
-            long = obj['longitude']
-            star = obj['stars']
-            addr = obj['full_address']
-            cats = obj['categories']
-            attr = obj['attributes']
-            Businesses.loc[len(Businesses)] = [b_id, name, lati, long, star, addr, cats, attr]
+            obj['full_address'] = re.sub('\s+', ' ', obj['full_address']).strip()
+            obj['postcode'] = find_postcode(obj['full_address'])
+            obj['attributes'] = clean_attributes(obj['attributes'])
+            Businesses = Businesses.append(obj, ignore_index=True)
+
+            if i % 300 == 0 or i == (total - 1):
+                sys.stdout.write('\r' + filename + ': ' + str(round(float(i + 1)/total * 100,2)) + '%')
+                sys.stdout.flush()
+            if i == (total - 1):
+                print " ... Done"
+
+def clean_attributes(attrs):
+    attr_list = []
+    mapping = ["cheap", "moderate", "moderate", "expensive"]
+    for key, val in attrs.items():
+        if isinstance(val, bool):
+            if val:
+                attr_list.append(key)
+        elif isinstance(val, str) or isinstance(val, unicode):
+            attr_list.append(key + ' ' + val)
+        elif isinstance(val, dict):
+            arr = clean_attributes(val)
+            attr_list.extend([key + ' ' + v for v in arr])
+        elif isinstance(val, int) and key.lower().find("price") > -1:
+            attr_list.append(key + " " + mapping[val-1] + " " + "$"*val)
+        else:
+            print "OTHER: ", type(val), key, val
+    return attr_list
+
+def find_postcode(addr):
+    postcode = POSTCODE.findall(addr)
+    if postcode:
+        postcode =  postcode[-1]
+    else:
+        postcode = ""
+    return postcode
 
 def read_reviews(filename):
+    global Reviews
+    total = file_len(filename)
     with open(filename, 'r') as fp:
-        for line in fp.readlines():
+        for i, line in enumerate(fp.readlines()):
             obj = json.loads(line)
-            b_id = obj['business_id']
-            text = obj['text']
-            vote = obj['votes']
-            Reviews.loc[len(Reviews)] = [b_id, text, vote]
+            Reviews = Reviews.append(obj, ignore_index=True)
+
+            if i % 1000 == 0 or i == (total - 1):
+                sys.stdout.write('\r' + filename + ': ' + str(round(float(i + 1)/total * 100,2)) + '%')
+                sys.stdout.flush()
+            if i == (total - 1):
+                print " ... Done"
 
 def read_tips(filename):
+    global Tips
+    total = file_len(filename)
     with open(filename, 'r') as fp:
-        for line in fp.readlines():
+        for i, line in enumerate(fp.readlines()):
             obj = json.loads(line)
-            b_id = obj['business_id']
-            text = obj['text']
-            like = obj['like']
-            Tips.loc[len(Tips)] = [b_id, text, like]
+            Tips.append(obj, ignore_index=True)
+
+            if i % 600 == 0 or i == (total - 1):
+                sys.stdout.write('\r' + filename + ': ' + str(round(float(i + 1)/total * 100,2)) + '%')
+                sys.stdout.flush()
+            if i == (total - 1):
+                print " ... Done"
 
 def handle_file(filename):
     if filename.find('business') > -1:
@@ -47,7 +93,16 @@ def handle_file(filename):
     if filename.find('tip') > -1:
         read_tips(filename)
 
+def file_len(filename):
+    p = subprocess.Popen(['wc', '-l', filename], stdout=subprocess.PIPE, 
+                         stderr=subprocess.PIPE)
+    result, err = p.communicate()
+    if p.returncode != 0:
+        raise IOError(err)
+    return int(result.strip().split()[0])
+
 if __name__ == '__main__':
+    
     file_or_dir = sys.argv[1]
     if not file_or_dir.endswith(".json"):
         for filename in os.listdir(file_or_dir):
@@ -55,9 +110,9 @@ if __name__ == '__main__':
     else:
         handle_file(file_or_dir)
 
-    print Businesses
-    print Reviews
-    print Tips
+#    print Businesses.ix[:,['full_address']]
+#    print Reviews
+#    print Tips
 
 
 
@@ -139,7 +194,7 @@ if __name__ == '__main__':
 #         for vs in range(len(tipni)):
 #             f.write(str(like_tip.iloc[vs])+ '\n')
 #             tipss = "".join([s for s in tipnis.splitlines(True) if s.strip("\r\n")])
-# 	    tipss = STOP.sub('', tipss.replace('\n', ' '))
+#	    tipss = STOP.sub('', tipss.replace('\n', ' '))
 #             f.write(tipss +'\n')
 #     except:
 #         f.write('0\n')
@@ -154,7 +209,7 @@ if __name__ == '__main__':
 #             f.write(str(likes.iloc[vals]['useful'])+'\n')
 #             r = Reviews.ix[ixx[vals]]
 #             revs = "".join([s for s in r.splitlines(True) if s.strip("\r\n")])
-# 	    revs = STOP.sub('', revs.replace('\n', ' '))
+#	    revs = STOP.sub('', revs.replace('\n', ' '))
 #             f.write(revs+'\n')
 
 #     except:
