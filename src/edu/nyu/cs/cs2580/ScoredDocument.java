bd3e153d9851ec37e0570662ee199f7b2cae41b6
Arpit Jain <arpit.jain2811@gmail.com>
2014-12-12 05:09:29 -0500

    display num_reviews

--- src/edu/nyu/cs/cs2580/ScoredDocument.java
+++ src/edu/nyu/cs/cs2580/ScoredDocument.java
@@ -32,6 +32,7 @@ class ScoredDocument implements Comparable<ScoredDocument> {
 		json_buf.put("address", get_doc().get_address());
 		json_buf.put("city", get_doc().getCity());
 		json_buf.put("url", get_doc().getUrl());
+		json_buf.put("num_reviews", get_doc().get_num_Reviews());
 		return json_buf.toJSONString();
 	}
 
